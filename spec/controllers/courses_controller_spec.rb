8463ae1aa235927483d98c87941da476ebac9e51
Yang Liu <byliuyang11@gmail.com>
2017-02-12 16:32:27 -0500

    Update loads all the courses for under given school into @courses test

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -3,15 +3,20 @@ require 'rails_helper'
 RSpec.describe CoursesController, type: :controller do
   describe "Get #index" do
     it "reponses successfully with an HTTP 200 status code" do
-      get :index
+      get :index, :school_id => 1
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
-    it "loads all the courses into @courses" do
-      course1, course2 = Course.create!, Course.create!
-      get :index
+    it "loads all the courses for under given school into @courses" do
+      school1, school2 = School.create!, School.create!
+
+      course1, course2, course3 =  school1.courses.create!, school1.courses.create, school2.courses.create
+      get :index, :school_id => school1.id
       expect(assigns(:courses)).to match_array([course1, course2])
+
+      get :index, :school_id => school2.id
+      expect(assigns(:courses)).to match_array([course3])
     end
   end
 end

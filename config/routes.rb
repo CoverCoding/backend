717040935edfe075c6e26b5723767fda8709bd4a
Yang Liu <byliuyang11@gmail.com>
2017-02-12 16:30:36 -0500

    Pass courses controller responses successfully with an HTTP 200 status code test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :schools
+  resources :schools, only: [:index]
+  get 'schools/:school_id/courses' => 'courses#index'
+  # resources :courses, only: [:index]
 end
